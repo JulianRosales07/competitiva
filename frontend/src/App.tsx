51410f997c74ec4e7b18b73bc0c76ad407594eae
JFreddy21 <jfrodriguez222@umariana.edu.co>
2025-09-01 22:26:15 -0500

    feat: actualizar rutas y eliminar componentes relacionados con equipaje

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import EquipajePage from "./pages/equipajePages";
+
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<EquipajePage />} />
+    
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
